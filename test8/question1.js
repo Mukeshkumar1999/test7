75798c6b9ef980f35500f7911649ef9a2b942bc9
Mukeshkumar1999 <92779777+Mukeshkumar1999@users.noreply.github.com>
2022-06-12 16:51:16 +0530

    Add files via upload

---
+++ test8/question1.js
@@ -0,0 +1,10 @@
+let varlist = ['geek', 'geekster', 'geeky'];
+console.log(varlist[1]);
+console.log(varlist.length);
+// let value = varlist.indexOf(1);
+// varlist.shift(1)
+// varlist.shift(1);
+// remove = varlist.splice(1, 0);
+varlist.pop()
+console.log(varlist);
+
